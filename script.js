e32c1f4b01383c92c2ae1ab37d1ff630b72e35c8
Pavel <Pavelnik82@gmail.com>
2020-04-20 18:32:37 +0300

    это двадцатимиллионый коммит

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log('string'*9);
+
+console.log(400/2);
+ 
